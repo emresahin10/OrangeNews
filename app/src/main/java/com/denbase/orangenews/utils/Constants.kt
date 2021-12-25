734e92fe9e2d4197fac3df6965cdc21f3a076d39
Emre Şahin <47294155+emresahin10@users.noreply.github.com>
2021-12-25 22:29:57 +0300

    Update Constants.kt

--- app/src/main/java/com/denbase/orangenews/utils/Constants.kt
+++ app/src/main/java/com/denbase/orangenews/utils/Constants.kt
@@ -2,7 +2,7 @@ package com.denbase.orangenews.utils
 
 class Constants {
     companion object{
-        val API_KEY = "c092edc3945c4d2d9cc8ee1a12a8a734"
+        val API_KEY = "apikey"
         val BASE_URL = "https://newsapi.org/v2/"
         val SPLASH_TIME = 2000L
     }
